58a8b444bde6ec3b172c5b8d0a5d926b05b470a2
hmk10 <halitmk@gmail.com>
2022-07-07 21:23:26 -0400

    I add 3 new classes that are very important

---
+++ src/FourthClass.java
@@ -0,0 +1,9 @@
+public class FourthClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("98765");
+
+    }
+
+}
---
+++ src/ThirdClass.java
@@ -0,0 +1,9 @@
+public class ThirdClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("123456");
+
+    }
+
+}
